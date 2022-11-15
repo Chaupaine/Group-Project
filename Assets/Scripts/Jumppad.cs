05e351a9baa8cd09610ba28b3f4095f83b421668
Chaupaine <92127745+Chaupaine@users.noreply.github.com>
2022-11-15 02:48:23 -0800

    new boost pad
    speeds will vary based on the camera's position. if the speed pad is tilted, face the tilted direction for maximum speed

--- Assets/Scripts/Jumppad.cs
+++ Assets/Scripts/Jumppad.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Jumppad : MonoBehaviour
 {
-    //when object collides with the object with this script, push away the object
     private void OnCollisionStay(Collision other)
     {
         other.collider.GetComponent<Rigidbody>().AddForce(Vector3.up * 300);

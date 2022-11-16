65aa4c0f5c21079f9798bec5dcacd6f43fb2afff
eduardo-barraza <90723239+eduardo-barraza@users.noreply.github.com>
2022-11-16 11:17:24 -0800

    Update PlayerController.cs
    updated it to closer reflect bc branch

--- Assets/Scripts/TestScripts_v1/PlayerController.cs
+++ Assets/Scripts/TestScripts_v1/PlayerController.cs
@@ -2,50 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// public class PlayerController : MonoBehaviour
-// {
-//     public float moveSpeed;
-//     public float jumpForce;
-//     public float drag;
-//     public bool isGrounded;
-
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         Vector3 direction = new Vector3(0,0,0);
-
-//         Vector3 forward = Camera.main.transform.TransformDirection(Vector3.forward);
-//         forward.y = 0;
-
-//         Vector3 forwardForce = forward * Input.GetAxis("Vertical") * drag;
-
-//         Vector3 right = Camera.main.transform.TransformDirection(Vector3.right);
-
-//         Vector3 rightForce = right * Input.GetAxis("Horizontal") * drag;
-//         if (isGrounded) 
-//         {
-//             direction += forwardForce;
-//             direction += rightForce;            
-//             if (Input.GetKeyDown(KeyCode.Space))
-//             {
-//                 GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-//                 isGrounded = false;
-//             }
-//         }
-//         GetComponent<Rigidbody>().AddForce(direction.normalized * moveSpeed * drag);
-//     }
-    
-//     void OnCollisionStay()
-//     {
-//         isGrounded = true;
-//     }
-
-//     void OnCollisionExit()
-//     {
-//         isGrounded = false;
-//     }
-// }
-
 public class PlayerController : MonoBehaviour {
 
 	public float speed = 1000f;
@@ -54,6 +10,9 @@ public class PlayerController : MonoBehaviour {
     private Rigidbody rb;
     public Vector3 jump = new Vector3(0,4,0);
     public float jumpForce = 2.0f;
+    public AudioClip jumpSound;
+    // public ParticleSystem dirtParticles;
+    // public ParticleSystem explosionParticles;
     
     void Start()
     {
@@ -67,12 +26,18 @@ public class PlayerController : MonoBehaviour {
 		movementVector = new Vector3(movementHorizontal, 0.0f, movementVertical);
         movementVector = Camera.main.transform.TransformDirection(movementVector);
 
+        // only if you dont want to be able to control the ball while in midair (still testing out)
+        // rb.AddForce(movementVector * speed * Time.deltaTime);
+
         if (isGrounded)
         {
+            //only if you dont want to be able to control the ball while in midair
 		    rb.AddForce(movementVector * speed * Time.deltaTime);
 
             if (Input.GetKeyDown(KeyCode.Space))
             {
+                //play jump sound
+                AudioSource.PlayClipAtPoint(jumpSound, transform.position);
                 rb.AddForce(jump * jumpForce, ForceMode.Impulse);
                 isGrounded = false;
             }

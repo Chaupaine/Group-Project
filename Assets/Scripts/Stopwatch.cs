de620eabf548e578b8977cce7aac69a80f4ee6b9
eduardo-barraza <90723239+eduardo-barraza@users.noreply.github.com>
2022-12-15 21:40:55 -0800

    Update Stopwatch.cs
    no more errors, handles each level appropriately

--- Assets/Scripts/Stopwatch.cs
+++ Assets/Scripts/Stopwatch.cs
@@ -24,20 +24,36 @@ public class Stopwatch : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // if first level, set total time to 0
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            PlayerPrefs.SetFloat("TotalTime", 0f);
+        }
+
         totalTime = PlayerPrefs.GetFloat("TotalTime", 0f);
         currentTime = PlayerPrefs.GetFloat("TotalTime", 0f);
 
+        // if last level, set total time text
+        if (SceneManager.GetActiveScene().buildIndex == SceneManager.sceneCountInBuildSettings - 1)
+        {
+            totalTimerText.text = "Total Time: " + totalTime.ToString("0.00") + " s";
+        }
         // set total time text
-        totalTimerText.text = "Total Time: " + totalTime.ToString("0.00") + " s";
+        // totalTimerText.text = "Total Time: " + totalTime.ToString("0.00") + " s";
     }
 
     // Update is called once per frame
     void Update()
     {
         currentTime = countDown ? currentTime -= Time.deltaTime : currentTime += Time.deltaTime;
-        SetTimerText();
-        timerText.color = Color.red;
-        PlayerPrefs.SetFloat("TotalTime", currentTime);
+
+        // if not last level, update timer text
+        if (SceneManager.GetActiveScene().buildIndex < SceneManager.sceneCountInBuildSettings - 1)
+        {
+            SetTimerText();
+            timerText.color = Color.red;
+            PlayerPrefs.SetFloat("TotalTime", currentTime);
+        }
     }
 
     private void SetTimerText() {
@@ -47,6 +63,10 @@ public class Stopwatch : MonoBehaviour
     private void OnActiveSceneChanged(Scene oldScene, Scene newScene)
     {
         float totalTime = PlayerPrefs.GetFloat("TotalTime");
-        timerText.text = "Total Time: " + totalTime.ToString("0.00") + " s";
+
+        
+        if (SceneManager.GetActiveScene().buildIndex < SceneManager.sceneCountInBuildSettings - 1) {
+            timerText.text = "Total Time: " + totalTime.ToString("0.00") + " s";
+        }
     }
 }
